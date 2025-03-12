6f20ea34204425ce0e887be9b07b325e8816c6a9
Henry Tsai <henry88002605@gmail.com>
2025-03-12 15:27:33 +1000

    methods

--- Program.cs
+++ Program.cs
@@ -1,11 +1,39 @@
 ﻿internal class Program
 {
-    public static void Main(string[] args)
+    private static void Main(string[] args)
     {
       // syntax Console.WriteLine("Hello, World!!!");
+      //  C# strick type
+      // var is a good way to declare a variable
       var name = "Henry";
       Console.WriteLine($"Hello, {name}!!!");
 
+      // Methods
+      int a = 1;
+      int b = 2;
 
+      // int c = AddNumbers(1, 2, 3, 4);
+      // Console.WriteLine(c);
+
+      Console.WriteLine(IsEven(2));
+
+    }
+
+    // public static int AddNumbers(int a, int b = 5)
+    // {
+    //   return a + b;
+    // }
+
+    // public static int AddNumbers(params int[] integers)
+    // {
+
+    // }
+
+    public static bool IsEven(int number)
+    {
+      if (number % 2 == 0) {
+        return true;
+      }
+      return false;
     }
 }
