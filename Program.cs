a7a9ced65b9671ee3c2601179172490d8834f686
Henry Tsai <henry88002605@gmail.com>
2025-03-12 15:03:45 +1000

    practice c# - variable

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+      // syntax Console.WriteLine("Hello, World!!!");
+      var name = "Henry";
+      Console.WriteLine($"Hello, {name}!!!");
+
+
+    }
+}
