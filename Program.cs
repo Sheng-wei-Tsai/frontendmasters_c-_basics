04684879adf146a45176ca8a2866c9972221d1ae
Henry Tsai <henry88002605@gmail.com>
2025-03-12 16:07:03 +1000

    enums and tuple

--- Program.cs
+++ Program.cs
@@ -3,15 +3,34 @@
   private static void Main(string[] args)
   {
     // build-in types
-    // Datetime
-    Console.WriteLine(DateTime.Now);
-    // If you're using DateTime.UtcNow, you're using the current date and time in UTC (Coordinated Universal Time). Avoid using DateTime.Now in pretty much all cases for production code.
-    Console.WriteLine(DateTime.UtcNow);
+    // int[]  arrayOfIntegers = [1, 2, 3, 4, ];
+    // // Console.WriteLine(arrayOfIntegers.Length);
 
-    // string is mutable
-    string name = "Henry";
-    name = name.ToUpper();
-    Console.WriteLine(name);
+
+    // foreach( char character in "Hello World!")
+    // {
+    //   Console.Write(character);
+    // }
+    // Tuple
+    var personInfo = (Age: 30, Name: "Alice", IsEmployed: true);
+    Console.WriteLine(personInfo.Age);
+    Console.WriteLine(personInfo.Name);
+    Console.WriteLine(personInfo.IsEmployed);
+
+
+    // var employeeType = EmployeeType.Developer;
+    // Console.WriteLine(employeeType);
+    foreach( var e in Enum.GetValues(typeof(EmployeeType)))
+    Console.WriteLine(e);
+  }
+  // Enums
+  enum EmployeeType
+  {
+    Manager = 2,
+    Developer = 3,
+    Designer = 4,
+    Supervisor = 5,
+    Worker = 6,
   }
 
 }
